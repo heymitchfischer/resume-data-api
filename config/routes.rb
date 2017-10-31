0987f4ada994be67bb3e2df3215000869a141dd6
agoldwater <dmgldwtr@gmail.com>
2017-10-30 19:59:53 -0500

    fixed routes and updated edu and skills controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,33 +3,33 @@ Rails.application.routes.draw do
   namespace :api do
     get '/students' => 'students#index'
     post '/students' => 'students#create'
-    get '/students' => 'students#show'
-    patch '/students' => 'students#update'
-    delete '/students' => 'students#destroy'
+    get '/students/:id' => 'students#show'
+    patch '/students/:id' => 'students#update'
+    delete '/students/:id' => 'students#destroy'
 
     get '/capstones' => 'capstones#index'
     post '/capstones' => 'capstones#create'
-    get '/capstones' => 'capstones#show'
-    patch '/capstones' => 'capstones#update'
-    delete '/capstones' => 'capstones#destroy'
+    get '/capstones/:id' => 'capstones#show'
+    patch '/capstones/:id' => 'capstones#update'
+    delete '/capstones/:id' => 'capstones#destroy'
 
     get '/experiences' => 'experiences#index'
     post '/experiences' => 'experiences#create'
-    get '/experiences' => 'experiences#show'
-    patch '/experiences' => 'experiences#update'
-    delete '/experiences' => 'experiences#destroy'
+    get '/experiences/:id' => 'experiences#show'
+    patch '/experiences/:id' => 'experiences#update'
+    delete '/experiences/:id' => 'experiences#destroy'
 
     get '/educations' => 'educations#index'
     post '/educations' => 'educations#create'
-    get '/educations' => 'educations#show'
-    patch '/educations' => 'educations#update'
-    delete '/educations' => 'educations#destroy'
+    get '/educations/:id' => 'educations#show'
+    patch '/educations/:id' => 'educations#update'
+    delete '/educations/:id' => 'educations#destroy'
 
     get '/skills' => 'skills#index'
     post '/skills' => 'skills#create'
-    get '/skills' => 'skills#show'
-    patch '/skills' => 'skills#update'
-    delete '/skills' => 'skills#destroy'
+    get '/skills/:id' => 'skills#show'
+    patch '/skills/:id' => 'skills#update'
+    delete '/skills/:id' => 'skills#destroy'
   end
   
 end
